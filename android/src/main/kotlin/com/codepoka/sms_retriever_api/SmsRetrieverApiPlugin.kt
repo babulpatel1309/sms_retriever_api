46b7dc2dfe639d5d27218ea4eaf208d2bec44941
Harsh Patel <harsh@trentiums.com>
2020-11-25 13:13:30 +0530

    - Added ability to return only code.

--- android/src/main/kotlin/com/codepoka/sms_retriever_api/SmsRetrieverApiPlugin.kt
+++ android/src/main/kotlin/com/codepoka/sms_retriever_api/SmsRetrieverApiPlugin.kt
@@ -13,98 +13,107 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import com.google.android.gms.auth.api.phone.SmsRetriever;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.Status;
-
-class SmsRetrieverApiPlugin(private val context: Activity): MethodCallHandler {
-  private var receiver: MySMSBroadcastReceiver? = null
-  var sms: String? = null
-  var result: MethodChannel.Result? = null
-
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "com.codepoka.sms_retriever_api")
-      channel.setMethodCallHandler(SmsRetrieverApiPlugin(registrar.activity()))
-    }
-  }
-
-  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-    when {
-      call.method == "getAppSignature" -> {
-        val signature = AppSignatureHelper(this.context).getAppSignatures()[0]
-        result.success(signature);
-      }
-      call.method == "startListening" -> {
-        receiver = MySMSBroadcastReceiver()
-        this.context.registerReceiver(receiver, IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION))
-        startListening()
-        this.result = result;
-      }
-      call.method == "stopListening" -> unregister()
-      else -> result.notImplemented()
-    }
-  }
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 
-  private fun startListening() {
-    // Get an instance of SmsRetrieverClient, used to start listening for a matching
-    // SMS message.
-    val client = SmsRetriever.getClient(this.context /* context */)
+class SmsRetrieverApiPlugin(private val context: Activity) : MethodCallHandler {
+    private var receiver: MySMSBroadcastReceiver? = null
+    var sms: String? = null
+    var result: MethodChannel.Result? = null
 
-    // Starts SmsRetriever, which waits for ONE matching SMS message until timeout
-    // (5 minutes). The matching SMS message will be sent via a Broadcast Intent with
-    // action SmsRetriever#SMS_RETRIEVED_ACTION.
-    val task = client.startSmsRetriever()
-
-    // Listen for success/failure of the start Task. If in a background thread, this
-    // can be made blocking using Tasks.await(task, [timeout]);
-    task.addOnSuccessListener {
-      // Successfully started retriever, expect broadcast intent
-      Log.e(javaClass::getSimpleName.name, "task started")
-
-    }
-
-    task.addOnFailureListener {
-      // Failed to start retriever, inspect Exception for more details
-      Log.e(javaClass::getSimpleName.name, "task starting failed")
-      // ...
-    }
-
-
-  }
-
-  private fun unregister() {
-    this.context.unregisterReceiver(receiver);
-  };
-
-
-  /**
-   * BroadcastReceiver to wait for SMS messages. This can be registered either
-   * in the AndroidManifest or at runtime.  Should filter Intents on
-   * SmsRetriever.SMS_RETRIEVED_ACTION.
-   */
-  inner class MySMSBroadcastReceiver : BroadcastReceiver() {
-
-
-    override fun onReceive(context: Context, intent: Intent) {
-      if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
-        val extras = intent.extras
-        val status = extras!!.get(SmsRetriever.EXTRA_STATUS) as Status
-
-        when (status.statusCode) {
-          CommonStatusCodes.SUCCESS -> {
-            // Get SMS message contents
-            sms = extras.get(SmsRetriever.EXTRA_SMS_MESSAGE) as String
-            result?.success(sms)
-          }
-
-          CommonStatusCodes.TIMEOUT -> {
-          }
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "com.codepoka.sms_retriever_api")
+            channel.setMethodCallHandler(SmsRetrieverApiPlugin(registrar.activity()))
         }
-        // Extract one-time code from the message and complete verification
-        // by sending the code back to your server.
-        // Waiting for SMS timed out (5 minutes)
-        // Handle the error ...
-      }
     }
-  }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when {
+            call.method == "getAppSignature" -> {
+                val signature = AppSignatureHelper(this.context).getAppSignatures()[0]
+                result.success(signature);
+            }
+            call.method == "startListening" -> {
+                receiver = MySMSBroadcastReceiver()
+                this.context.registerReceiver(receiver, IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION))
+                startListening()
+                this.result = result;
+            }
+            call.method == "stopListening" -> unregister()
+            else -> result.notImplemented()
+        }
+    }
+
+
+    private fun startListening() {
+        // Get an instance of SmsRetrieverClient, used to start listening for a matching
+        // SMS message.
+        val client = SmsRetriever.getClient(this.context /* context */)
+
+        // Starts SmsRetriever, which waits for ONE matching SMS message until timeout
+        // (5 minutes). The matching SMS message will be sent via a Broadcast Intent with
+        // action SmsRetriever#SMS_RETRIEVED_ACTION.
+        val task = client.startSmsRetriever()
+
+        // Listen for success/failure of the start Task. If in a background thread, this
+        // can be made blocking using Tasks.await(task, [timeout]);
+        task.addOnSuccessListener {
+            // Successfully started retriever, expect broadcast intent
+            Log.e(javaClass::getSimpleName.name, "task started")
+
+        }
+
+        task.addOnFailureListener {
+            // Failed to start retriever, inspect Exception for more details
+            Log.e(javaClass::getSimpleName.name, "task starting failed")
+            // ...
+        }
+
+
+    }
+
+    private fun unregister() {
+        this.context.unregisterReceiver(receiver);
+    };
+
+
+    /**
+     * BroadcastReceiver to wait for SMS messages. This can be registered either
+     * in the AndroidManifest or at runtime.  Should filter Intents on
+     * SmsRetriever.SMS_RETRIEVED_ACTION.
+     */
+    inner class MySMSBroadcastReceiver : BroadcastReceiver() {
+
+
+        override fun onReceive(context: Context, intent: Intent) {
+            if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
+                val extras = intent.extras
+                val status = extras!!.get(SmsRetriever.EXTRA_STATUS) as Status
+
+                when (status.statusCode) {
+                    CommonStatusCodes.SUCCESS -> {
+                        // Get SMS message contents
+                        val message = extras.get(SmsRetriever.EXTRA_SMS_MESSAGE) as String
+                        val pattern: Pattern = Pattern.compile("\\d{4,6}")
+                        val matcher: Matcher = pattern.matcher(message)
+                        if (matcher.find()) {
+                            result.success(matcher.group(0))
+                        } else {
+                            result.success(message)
+                        }
+                    }
+
+                    CommonStatusCodes.TIMEOUT -> {
+                    }
+                }
+                // Extract one-time code from the message and complete verification
+                // by sending the code back to your server.
+                // Waiting for SMS timed out (5 minutes)
+                // Handle the error ...
+            }
+        }
+    }
 }
